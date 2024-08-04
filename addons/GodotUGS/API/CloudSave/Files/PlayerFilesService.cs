9dab51a15a5f4a9b9708422851a2a525716e2fe5
Tirt <75492176+Tirtstan@users.noreply.github.com>
2024-08-04 12:04:29 +0200

    cloud save interface pattern

--- addons/GodotUGS/API/CloudSave/Files/PlayerFilesService.cs
+++ addons/GodotUGS/API/CloudSave/Files/PlayerFilesService.cs
@@ -2,6 +2,7 @@ namespace Unity.Services.CloudSave.Internal;
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -13,7 +14,80 @@ using Unity.Services.CloudSave.Internal.Models;
 using Unity.Services.CloudSave.Models;
 using Unity.Services.Core;
 
-public class PlayerFilesService
+public interface IPlayerFilesService
+{
+    /// <summary>
+    /// Returns all player-scoped files stored in Cloud Save for the logged in player.
+    /// Throws a CloudSaveException with a reason code and explanation of what happened.
+    /// This method includes pagination.
+    /// </summary>
+    /// <returns>A list of file metadata for the files stored in Cloud Save for the logged in player.</returns>
+    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
+    public Task<List<FileItem>> ListAllAsync();
+
+    /// <summary>
+    /// Returns the metadata of a file stored in Cloud Save for the logged in player.
+    /// Throws a CloudSaveException with a reason code and explanation of what happened.
+    /// </summary>
+    /// <returns>The metadata of the specified file stored in Cloud Save for the logged in player</returns>
+    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
+    public Task<FileItem> GetMetadataAsync(string key);
+
+    /// <summary>
+    /// Upload a player-scoped file to the Cloud Save service, overwriting if the file already exists.
+    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
+    /// Throws a CloudSaveException with a reason code and explanation of what happened.
+    /// </summary>
+    /// <param name="key">The key at which to upload the file</param>
+    /// <param name="stream">The path to the file (DO NOT USE res:// or user://)</param>
+    /// <param name="options">Options object with "WriteLock", the expected stored writeLock of the file - if this value is provided and is not a match then the operation will not succeed. If it is not provided then the operation will be performed regardless of the stored writeLock value.</param>
+    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
+    /// <exception cref="InvalidOperationException">Thrown if request is invalid.</exception>
+    public Task SaveAsync(string key, Stream stream, WriteLockOptions options = null);
+
+    /// <summary>
+    /// Upload a player-scoped file to the Cloud Save service, overwriting if the file already exists.
+    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
+    /// Throws a CloudSaveException with a reason code and explanation of what happened.
+    /// </summary>
+    /// <param name="key">The key at which to upload the file</param>
+    /// <param name="bytes">The byte array containing the file data</param>
+    /// <param name="options">Options object with "WriteLock", the expected stored writeLock of the file - if this value is provided and is not a match then the operation will not succeed. If it is not provided then the operation will be performed regardless of the stored writeLock value.</param>
+    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
+    public Task SaveAsync(string key, byte[] bytes, WriteLockOptions options = null);
+
+    /// <summary>
+    /// Upload a player-scoped file to the Cloud Save service, overwriting if the file already exists.
+    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
+    /// Throws a CloudSaveException with a reason code and explanation of what happened.
+    /// </summary>
+    /// <returns>A Stream containing the downloaded file data</returns>
+    /// <param name="key">The key of the saved file to be loaded.</param>
+    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
+    public Task<Stream> LoadStreamAsync(string key);
+
+    /// <summary>
+    /// Upload a player-scoped file to the Cloud Save service, overwriting if the file already exists.
+    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
+    /// Throws a CloudSaveException with a reason code and explanation of what happened.
+    /// </summary>
+    /// <returns>A byte array containing the downloaded file data</returns>
+    /// <param name="key">The key of the saved file to be loaded.</param>
+    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
+    public Task<byte[]> LoadBytesAsync(string key);
+
+    /// <summary>
+    /// Delete a player-scoped file form the Cloud Save service.
+    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
+    /// Throws a CloudSaveException with a reason code and explanation of what happened.
+    /// </summary>
+    /// <param name="key">The key of the saved file to be deleted.</param>
+    /// <param name="options">Options object with "WriteLock", the expected stored writeLock of the file - if this value is provided and is not a match then the operation will not succeed. If it is not provided then the operation will be performed regardless of the stored writeLock value.</param>
+    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
+    public Task DeleteAsync(string key, WriteLockOptions options = null);
+}
+
+public class PlayerFilesService : IPlayerFilesService
 {
     private RestClient playerFilesClient;
     private const string PlayerFilesURL = "https://cloud-save.services.api.unity.com/v1/files";
@@ -37,13 +111,6 @@ public class PlayerFilesService
         playerFilesClient.AddDefaultHeaders(UnityServices.Instance.DefaultHeaders);
     }
 
-    /// <summary>
-    /// Returns all player-scoped files stored in Cloud Save for the logged in player.
-    /// Throws a CloudSaveException with a reason code and explanation of what happened.
-    /// This method includes pagination.
-    /// </summary>
-    /// <returns>A list of file metadata for the files stored in Cloud Save for the logged in player.</returns>
-    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
     public async Task<List<FileItem>> ListAllAsync()
     {
         var request = new RestRequest($"projects/{ProjectId}/players/{PlayerId}/items")
@@ -63,12 +130,6 @@ public class PlayerFilesService
         }
     }
 
-    /// <summary>
-    /// Returns the metadata of a file stored in Cloud Save for the logged in player.
-    /// Throws a CloudSaveException with a reason code and explanation of what happened.
-    /// </summary>
-    /// <returns>The metadata of the specified file stored in Cloud Save for the logged in player</returns>
-    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
     public async Task<FileItem> GetMetadataAsync(string key)
     {
         var request = new RestRequest($"/projects/{ProjectId}/players/{PlayerId}/items/{key}/metadata")
@@ -83,20 +144,10 @@ public class PlayerFilesService
             throw new CloudSaveException(response.Content, response.ErrorMessage, response.ErrorException);
     }
 
-    /// <summary>
-    /// Upload a player-scoped file to the Cloud Save service, overwriting if the file already exists.
-    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
-    /// Throws a CloudSaveException with a reason code and explanation of what happened.
-    /// </summary>
-    /// <param name="key">The key at which to upload the file</param>
-    /// <param name="stream">The path to the file (DO NOT USE res:// or user://)</param>
-    /// <param name="options">Options object with "WriteLock", the expected stored writeLock of the file - if this value is provided and is not a match then the operation will not succeed. If it is not provided then the operation will be performed regardless of the stored writeLock value.</param>
-    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
-    /// <exception cref="InvalidOperationException">Thrown if request is invalid.</exception>
-    public async Task SaveAsync(string key, System.IO.Stream stream, WriteLockOptions options = null)
+    public async Task SaveAsync(string key, Stream stream, WriteLockOptions options = null)
     {
         byte[] bytes;
-        using (var memoryStream = new System.IO.MemoryStream())
+        using (var memoryStream = new MemoryStream())
         {
             stream.CopyTo(memoryStream);
             bytes = memoryStream.ToArray();
@@ -105,50 +156,17 @@ public class PlayerFilesService
         await Save(key, bytes, options);
     }
 
-    /// <summary>
-    /// Upload a player-scoped file to the Cloud Save service, overwriting if the file already exists.
-    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
-    /// Throws a CloudSaveException with a reason code and explanation of what happened.
-    /// </summary>
-    /// <param name="key">The key at which to upload the file</param>
-    /// <param name="bytes">The byte array containing the file data</param>
-    /// <param name="options">Options object with "WriteLock", the expected stored writeLock of the file - if this value is provided and is not a match then the operation will not succeed. If it is not provided then the operation will be performed regardless of the stored writeLock value.</param>
-    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
     public async Task SaveAsync(string key, byte[] bytes, WriteLockOptions options = null) =>
         await Save(key, bytes, options);
 
-    /// <summary>
-    /// Upload a player-scoped file to the Cloud Save service, overwriting if the file already exists.
-    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
-    /// Throws a CloudSaveException with a reason code and explanation of what happened.
-    /// </summary>
-    /// <returns>A Stream containing the downloaded file data</returns>
-    /// <param name="key">The key of the saved file to be loaded.</param>
-    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
-    public async Task<System.IO.Stream> LoadStreamAsync(string key)
+    public async Task<Stream> LoadStreamAsync(string key)
     {
         var bytes = await Load(key);
-        return new System.IO.MemoryStream(bytes);
+        return new MemoryStream(bytes);
     }
 
-    /// <summary>
-    /// Upload a player-scoped file to the Cloud Save service, overwriting if the file already exists.
-    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
-    /// Throws a CloudSaveException with a reason code and explanation of what happened.
-    /// </summary>
-    /// <returns>A byte array containing the downloaded file data</returns>
-    /// <param name="key">The key of the saved file to be loaded.</param>
-    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
     public async Task<byte[]> LoadBytesAsync(string key) => await Load(key);
 
-    /// <summary>
-    /// Delete a player-scoped file form the Cloud Save service.
-    /// File name can only contain alphanumeric characters, dashes, and underscores and be up to a length of 255 characters.
-    /// Throws a CloudSaveException with a reason code and explanation of what happened.
-    /// </summary>
-    /// <param name="key">The key of the saved file to be deleted.</param>
-    /// <param name="options">Options object with "WriteLock", the expected stored writeLock of the file - if this value is provided and is not a match then the operation will not succeed. If it is not provided then the operation will be performed regardless of the stored writeLock value.</param>
-    /// <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
     public async Task DeleteAsync(string key, WriteLockOptions options = null)
     {
         var request = new RestRequest(
@@ -225,7 +243,7 @@ public class PlayerFilesService
 
         var response = await playerFilesClient.ExecuteAsync(request);
         if (response.IsSuccessful)
-            return response.RawBytes;
+            return response?.RawBytes ?? null;
         else
             throw new CloudSaveException(response.Content, response.ErrorMessage, response.ErrorException);
     }
